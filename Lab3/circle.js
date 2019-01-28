a4a39088173ea5572c2ee4d761c0fc1610f1053b
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-01-28 18:05:32 -0500

    Lab 3 Submission

--- Lab3/circle.js
+++ Lab3/circle.js
@@ -1,3 +1,8 @@
 export default class Circle {
-
+    constructor(color){
+        this.color = color;
+        this.x = (Math.random() * 600);
+        this.y = (Math.random() * 300);
+        this.radius = (Math.random() * (30-10) + 10);
+    }
 }
--- Lab3/game.js
+++ Lab3/game.js
@@ -1,3 +1,17 @@
 export default class Game {
-
+    constructor(maxPoints){
+        this.maxPoints = maxPoints;
+        this.points = 0;
+    }
+    isOver() {
+        if(this.points >= this.maxPoints) { return true;}
+        return false;
+    }
+    reset() {
+        this.points = 0;
+    }
+    hit() {
+        this.points++;
+    }
+    
 }
