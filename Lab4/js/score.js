85ef7711b217c8bb4ad71d176b052036c7030574
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-03-06 14:20:25 -0500

    Create addResult method Ref #2

--- Lab4/js/score.js
+++ Lab4/js/score.js
@@ -5,4 +5,25 @@ export default class Score {
 		this.stayWins = 0;
 		this.stayLosses = 0;
 	}
-}
+	addResult(action, result){
+		if (action == Score.ACTION_SWITCH) {
+		    if (result == Score.RESULT_WIN) {
+		        this.switchWins += 1;
+    		}else{
+    			this.switchLosses += 1;
+    		}
+		}
+		if (action == Score.ACTION_STAY){
+			if( result == Score.RESULT_WIN){
+				this.stayWins +=1;
+			}else{
+				this.stayLosses +=1;
+			}
+		}
+	}
+};
+
+Score.ACTION_SWITCH = 'switch';
+Score.RESULT_WIN = 'win';
+Score.ACTION_STAY = 'stay';
+Score.RESULT_LOSS = 'loss';
--- Lab4/test/score.spec.js
+++ Lab4/test/score.spec.js
@@ -7,6 +7,17 @@ describe('Score Instances', () => {
 	it('start with all counts set to zero', () => {
 		checkValuesAre(0,0,0,0);
 	});
+	it('increments using addResult method',()=>{
+		score.addResult(Score.ACTION_SWITCH, Score.RESULT_WIN);
+		checkValuesAre(1,0,0,0);
+		score.addResult(Score.ACTION_SWITCH, Score.RESULT_LOSS);
+		checkValuesAre(1,1,0,0);
+		score.addResult(Score.ACTION_STAY, Score.RESULT_WIN);
+		checkValuesAre(1,1,1,0);
+		score.addResult(Score.ACTION_STAY, Score.RESULT_LOSS);
+		checkValuesAre(1,1,1,1);
+
+	});
 	function checkValuesAre(switchWins,switchLosses,stayWins,stayLosses) {
 		expect(score.switchWins).to.equal(switchWins);
 		expect(score.switchLosses).to.equal(switchLosses);
