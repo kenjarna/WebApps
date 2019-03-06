4d842fa7359d961b99ce2df59491552164f1cdf7
Kenny Jarnagin <jarnagink19@hanover.edu>
2019-03-06 13:52:59 -0500

    Test Refactoring Ref #2

--- Lab4/test/score.spec.js
+++ Lab4/test/score.spec.js
@@ -2,12 +2,16 @@ let expect = chai.expect;
 import Score from '../js/score.js';
 
 describe('Score Instances', () => {
-   it('start with all counts set to zero', () => {
-      let score = new Score();
-      expect(score.switchWins).to.equal(0);
-      expect(score.switchLosses).to.equal(0);
-      expect(score.stayWins).to.equal(0);
-      expect(score.stayLosses).to.equal(0);
-   });
+	let score;
+	beforeEach(()=>{ score = new Score(); });
+	it('start with all counts set to zero', () => {
+		checkValuesAre(0,0,0,0);
+	});
+	function checkValuesAre(switchWins,switchLosses,stayWins,stayLosses) {
+		expect(score.switchWins).to.equal(switchWins);
+		expect(score.switchLosses).to.equal(switchLosses);
+		expect(score.stayWins).to.equal(stayWins);
+		expect(score.stayLosses).to.equal(stayLosses);
+	};
 
 });
