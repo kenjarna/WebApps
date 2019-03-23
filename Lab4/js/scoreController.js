9e63335e8d6aeea0cf30ab4a495f3abb469097e9
Kenny <jarnagink19@hanover.edu>
2019-03-23 19:43:52 -0400

    Finish Implementation of scorecard controller Close #5

--- Lab4/js/scoreController.js
+++ Lab4/js/scoreController.js
@@ -4,5 +4,14 @@ export default class ScorecardController {
 	    this.ui = ui;
 	    this.ui.initialize(score);
 	    this.score.on('change', (property, value) => this.modelChanged(property, value));
+	    this.ui.on(() => this.resetRequested());
 	}
+
+	modelChanged(property, value) {
+	   this.ui.update(property, value);
+	}
+	resetRequested() {
+		this.score.reset();
+	}
+
 }
--- Lab4/test/scoreController.spec.js
+++ Lab4/test/scoreController.spec.js
@@ -1,10 +1,11 @@
 import Score from '../js/score.js';
 import ScoreController from '../js/scoreController.js';
 import Spy from './spy.js';
+import Observable from '../js/observable.js';
 
 let expect = chai.expect;
 
-class FakeScorecardUI {
+class FakeScorecardUI extends Observable {
    initialize(o) {}
    update(field, newValue) {}
 }
@@ -31,5 +32,22 @@ describe('ScoreController instances', () => {
 	   new ScoreController(score, ui);
 	   expect(spy.numberOfCalls()).to.equal(1);
 	   expect(spy.argumentsOfCall(0)).to.deep.equal([score]);
+	});
+	it('call the ui update when the model triggers a change', done => {
+	    let spy = new Spy(ui, 'update', (field, value) => {
+	        expect(field).to.equal('switchWins');
+	        expect(value).to.equal(score.switchWins);
+	        done();
+    	});
+	    score.trigger('change', 'switchWins', score.switchWins);
+	});
+	it('register to listen to ui resetRequested', () => {
+	   let spy = new Spy(ui, 'on');
+	   new ScoreController(score,ui);
+	   expect(spy.numberOfCalls()).to.equal(1);
+	});
+	it('call the model reset method when the ui triggers resetRequested', (done) => {
+	   	let spy = new Spy(score, 'reset', done());
+	   	ui.trigger('resetRequested');
 	});
 });
